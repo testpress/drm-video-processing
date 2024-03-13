118f036bd658793e6ae9407d95b51a81d7bab8d1
MadheshKumarVJ <vjmadhesh@testpress.in>
2024-03-13 13:58:27 +0530

    fix: Add dependency setup script

---
+++ install.sh
@@ -0,0 +1,19 @@
+REQUIRED_PKG="ffmpeg"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+  echo "Installed ffmpeg"
+fi
+
+if ! which packager > /dev/null 2>&1; then
+  echo "Installing Shaka packager"
+  wget https://github.com/shaka-project/shaka-packager/releases/download/v2.6.1/packager-linux-x64
+  sudo mv packager-linux-x64 /usr/local/bin/packager
+  sudo chmod +rx /usr/local/bin/packager
+  echo "Shaka packager installation completed successfully"
+else
+  echo "shaka packager: already installed"
+fi
