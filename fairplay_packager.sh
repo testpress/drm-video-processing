01f067dc8a8c4168f20b49fd56ad86b1901a21ce
MadheshKumarVJ <vjmadhesh@testpress.in>
2024-03-14 17:05:54 +0530

    fix: Add fairplay_packager script

---
+++ fairplay_packager.sh
@@ -0,0 +1,20 @@
+# Read configuration from config.json
+JSON_FILE="config.json"
+CONTENT_ID=$(jq -r '.content_id' "$JSON_FILE")
+VIDEO_PATH=$(jq -r '.video_path' "$JSON_FILE")
+FAIRPLAY_KEY=$(jq -r '.fairplay_key' "$JSON_FILE")
+FAIRPLAY_IV=$(jq -r '.fairplay_iv' "$JSON_FILE")
+FAIRPLAY_URI=$(jq -r '.fairplay_uri' "$JSON_FILE")
+
+
+
+# Execute packager command
+packager -v=4 \
+  in="${VIDEO_PATH},stream=audio,output=audio.mp4" \
+  in="${VIDEO_PATH},stream=video,output=h264_360p.mp4" \
+  --keys "label=:key_id=${CONTENT_ID}:key=${FAIRPLAY_KEY}:iv=${FAIRPLAY_IV}" \
+  --protection_scheme cbcs \
+  --protection_systems Fairplay \
+  --hls_key_uri "$FAIRPLAY_URI" \
+  --clear_lead 0 \
+  --hls_master_playlist_output video.m3u8
